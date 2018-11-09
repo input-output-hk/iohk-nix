7fae72207ebce240394e4f65e83486f5cb4a1042
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-11-09 19:18:58 +1000

    Add pinned nixpkgs to common nix repo
    (cherry picked from commit d8757437c1b29c877b1089365c3690a9a138019c)

--- default.nix
+++ default.nix
@@ -1,10 +1,40 @@
-{
-  fetchNixpkgs = import ./fetch-nixpkgs.nix;
-  cleanSourceHaskell = import ./clean-source-haskell.nix;
-  haskellPackages = ./haskell-packages.nix;
-  maybeEnv = import ./maybe-env.nix;
-  jemallocOverlay = import ./overlays/jemalloc.nix;
-  getPackages = import ./get-packages.nix;
+{ config ? {}
+, system ? builtins.currentSystem
+}:
+
+rec {
+  commonLib = {
+    fetchNixpkgs = import ./fetch-nixpkgs.nix;
+    maybeEnv = import ./maybe-env.nix;
+  };
+
+  # Overlay which adds more functions to pkgs.lib
+  extraLib = self: super: {
+    lib = super.lib.extend (lib: _: {
+      cleanSourceHaskell = import ./clean-source-haskell.nix { inherit lib; };
+      getPackages = import ./get-packages.nix { inherit lib; };
+      commitIdFromGitRepo = import ./commit-id.nix { inherit lib; };
+    });
+  };
+
+  cardano-sl = rec {
+    pkgs = import (commonLib.fetchNixpkgs ./cardano-sl/nixpkgs-src.json) {
+      inherit config system;
+      overlays = [
+        (import ./overlays/jemalloc.nix)
+        extraLib
+      ];
+    };
+    haskellPackages = args: import ./haskell-packages.nix (args // { inherit pkgs; });
+  };
+
+  daedalus = {
+    pkgs = import (commonLib.fetchNixpkgs ./daedalus/nixpkgs-src.json) {
+      inherit config system;
+      # overlays = [ extraLib ]; # needs nixpkgs-18.09
+    };
+  };
+
   tests = {
     hlint = ./tests/hlint.nix;
     shellcheck = ./tests/shellcheck.nix;
