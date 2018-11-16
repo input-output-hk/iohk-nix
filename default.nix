11006c13ab397aa616eccb1418fe04f468c7dfbe
Samuel Leathers <samuel.leathers@iohk.io>
2018-11-16 09:39:38 -0500

    remove top-level daedalus/cardano-sl packages

--- default.nix
+++ default.nix
@@ -8,16 +8,15 @@
 
 let
   # Default nixpkgs-src.json to use
-  nixpkgsJsonDefault = ./nixpkgs-src.json;
+  nixpkgsJsonDefault = ./nixpkgs-pins/default-nixpkgs-src.json;
   nixpkgsJson = if (nixpkgsJsonOverride != "") then nixpkgsJsonOverride else
     (if (application != "") then (getNixpkgsJson application) else nixpkgsJsonDefault);
 
-  getNixpkgsJson = application: ./. + "/${application}/nixpkgs-src.json";
+  getNixpkgsJson = application: ./nixpkgs-pins + "/${application}-nixpkgs-src.json";
   jemallocOverlay = import ./overlays/jemalloc.nix;
 
   commonLib = rec {
     fetchNixpkgs = import ./fetch-nixpkgs.nix;
-    getPackages = import ./get-packages.nix;
     # equivalent of <nixpkgs> but pinned instead of system
     nixpkgs = fetchNixpkgs nixpkgsJson;
     getPkgs = { args ? {}, extraOverlays ? [] }: import (fetchNixpkgs nixpkgsJson) ({
@@ -25,20 +24,12 @@ let
       inherit config system;
     } // args);
     pkgs = getPkgs {};
+    getPackages = pkgs.callPackage ./get-packages.nix {};
     maybeEnv = import ./maybe-env.nix;
     cleanSourceHaskell = pkgs.callPackage ./clean-source-haskell.nix {};
     haskellPackages = import ./haskell-packages.nix;
+    commitIdFromGitRepo = pkgs.callPackage ./commit-id.nix {};
   };
-
-  # Overlay which adds more functions to pkgs.lib
-  extraLib = self: super: {
-    lib = super.lib.extend (lib: _: {
-      inherit (commonLib) cleanSourceHaskell;
-      getPackages = commonLib.pkgs.callPackage ./get-packages.nix {};
-      commitIdFromGitRepo = commonLib.pkgs.callPackage ./commit-id.nix {};
-    });
-  };
-
 
   tests = {
     hlint = ./tests/hlint.nix;
@@ -46,25 +37,7 @@ let
     stylishHaskell = ./tests/stylish-haskell.nix;
   };
 
-  # Specific package tooling. If you have to add something to iohk-nix
-  # that only applies to your project, do it here:
-
-  cardano-sl = rec {
-    pkgs = commonLib.getPkgs {
-      extraOverlays = [
-        extraLib
-      ];
-    };
-    haskellPackages = args: commonLib.pkgs.callPackage ./haskell-packages.nix (args // { });
-  };
-
-  daedalus = {
-    inherit (commonLib) pkgs;
-  };
-
-  # End specific packaging tooling
-
 in {
-  inherit tests daedalus cardano-sl application nixpkgsJson;
-  inherit (commonLib) pkgs jemallocOverlay haskellPackages fetchNixpkgs maybeEnv cleanSourceHaskell getPkgs nixpkgs;
+  inherit tests;
+  inherit (commonLib) pkgs jemallocOverlay haskellPackages fetchNixpkgs maybeEnv cleanSourceHaskell getPkgs nixpkgs commitIdFromGitRepo getPackages;
 }
