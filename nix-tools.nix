3e08c120d249ea67c3fbfa19d1ced1ab4283ef8b
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-12-12 14:12:26 +1000

    Enable fetchSubmodules for nix-tools git repo

--- nix-tools.nix
+++ nix-tools.nix
@@ -8,6 +8,7 @@ let
     repo = "nix-tools";
     rev = "b7835666bcf73c3fa50f5c59bfa4cac29ea8d626";
     sha256 = "1pnw10jvlb6gld6ja294yzf4mlnc8a0sxml3d0mhgi7ns4zr9ggy";
+    fetchSubmodules = true;
   };
   nix-tools = import src { inherit pkgs; };
 in
