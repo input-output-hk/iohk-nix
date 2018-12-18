3df208c72cbce3b893ae096f175527e0a4c7dcc7
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-12-18 21:06:20 +1000

    [DEVOPS-1179] nix-tools: fetchSubmodules = true no longer required

--- nix-tools.nix
+++ nix-tools.nix
@@ -8,7 +8,6 @@ let
     repo = "nix-tools";
     rev = "d4e4b74d539fee8e580e1531637db67053b62676";
     sha256 = "1y7rm6ds14hii97q34h1mdjfhk0mx2jxiyd295vsnncf3lvcrfn6";
-    fetchSubmodules = true;
   };
   nix-tools = import src { inherit pkgs; };
 in
