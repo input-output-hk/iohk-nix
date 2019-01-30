7e42c85ee21b83040a7ed7e952f378d241d3b8e8
Moritz Angermann <moritz.angermann@gmail.com>
2019-01-30 11:01:55 +0800

    change hackage/haskell/stackage repo

--- haskell-hackage-stackage.nix
+++ haskell-hackage-stackage.nix
@@ -12,7 +12,7 @@ let
 in rec {
   # all packages from hackage as nix expressions
   hackage = import (overrideWith "hackage"
-                    (pkgs.fetchFromGitHub { owner  = "angerman";
+                    (pkgs.fetchFromGitHub { owner  = "input-output-hk";
                                             repo   = "hackage.nix";
                                             rev    = "d4ef2536554d78c6adf368816a4476bc909fcd96";
                                             sha256 = "15siby9iwgi8bhkj83q1djvg6b6n4gj08n7yy66ccjck7fw4hbr8";
@@ -20,7 +20,7 @@ in rec {
                    ;
   # a different haskell infrastructure
   haskell = import (overrideWith "haskell"
-                    (pkgs.fetchFromGitHub { owner  = "angerman";
+                    (pkgs.fetchFromGitHub { owner  = "input-output-hk";
                                             repo   = "haskell.nix";
                                             rev    = "dae8025469d94e739aa52f23685bcd87840c72e3";
                                             sha256 = "0bj577aj2v7yl6d1l6rinp0bbn4k9m8k4jd46m4hspqaisfd58hg";
@@ -29,7 +29,7 @@ in rec {
 
   # the set of all stackage snapshots
   stackage = import (overrideWith "stackage"
-                     (pkgs.fetchFromGitHub { owner  = "angerman";
+                     (pkgs.fetchFromGitHub { owner  = "input-output-hk";
                                              repo   = "stackage.nix";
                                              rev    = "f58d5b78e7a40260c6142c79e52c2bf3ae9876b9";
                                              sha256 = "1nd9lfm016rlhw3133488f8v8x3lbxrld422gw8gcjhhfls3civn";
