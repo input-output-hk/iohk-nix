0e4be21b15899e9aefdd6f7383cf0dd12d0c1903
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2019-02-15 09:17:25 +1000

    Remove obsolete pins from updater script

--- haskell.nix
+++ haskell.nix
@@ -4,6 +4,7 @@ let
   overrideWith = import ./fetch-tarball-with-override.nix;
 
 in
+  # To update this pin, run ./pins/update-defaults.sh
   import (overrideWith "haskell" ./pins/haskell-nix.json) {
     inherit pkgs;
     # hackageSourceJSON = ./pins/hackage-nix.json;
