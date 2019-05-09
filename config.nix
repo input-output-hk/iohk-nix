1f40787e6855007f41762a1e830e2962bac2f6b9
Edward Amsden <edward@blackriversoft.com>
2019-05-08 21:07:57 -0400

    Explicitly disable enableRelocatedStaticLibs since it breaks iserv

--- config.nix
+++ config.nix
@@ -15,6 +15,7 @@
                           then "perf-cross-ncg"
                           else "perf-cross";
         enableShared = ps.stdenv.targetPlatform == ps.stdenv.hostPlatform;
+        enableRelocatedStaticLibs = false;
         enableIntegerSimple = false;
       };
     ghcDrvOverrides = drv: {
