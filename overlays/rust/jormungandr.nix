288c6449f7c8c33385792fcf6dd0ff79652cdb6f
Samuel Leathers <samuel.leathers@iohk.io>
2019-06-11 17:17:54 -0400

    jormungandr bump to master

--- overlays/rust/jormungandr.nix
+++ overlays/rust/jormungandr.nix
@@ -12,12 +12,12 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "input-output-hk";
     repo = "jormungandr";
-    rev = "c53a896774d30b357a6dbbabe909a14509fa078a";
-    sha256 = "1pwkb8xrmzpwnr5fw1yns7m3qybpc67na3kzwsvqcb4xbnb57yyy";
+    rev = "88f3421b0665f36782a70485fce4922935e785dc";
+    sha256 = "0xcg2vzl5zc0hzq339r4sxc2nzj9vc28xhplcfxf1k1x0xis79yq";
     fetchSubmodules = true;
   };
 
-  cargoSha256 = "1n0qg0nqhs653xc0d2iy4dd0shzbdxklxydxh674dkssrb28bmab";
+  cargoSha256 = "1na2fqf90x34c2qix2i4yjdimjb3k6khfk7m1ig5vqc5nv6y7ahy";
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ sqlite protobuf openssl ];
   PROTOC = "${protobuf}/bin/protoc";
