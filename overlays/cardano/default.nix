b9e71042d4e42816a541ab75c8d98aa909b31b31
Samuel Leathers <samuel.leathers@iohk.io>
2019-07-31 15:28:09 -0400

    fix package names

--- overlays/cardano/default.nix
+++ overlays/cardano/default.nix
@@ -5,9 +5,9 @@ self: super: let
     rev = "v2019-07-24";
     sha256 = "11j3an4i6kvk9h71hz1cngns2a47cmv38bsr3qyagcr1829rlrxl";
   }) {};
-  cardanoWalletJormungandr = wallet.cardano-wallet-jormungandr;
-  cardanoWalletHttpBridge = wallet.cardano-wallet-http-bridge;
+  cardano-wallet-jormungandr = wallet.cardano-wallet-jormungandr;
+  cardano-wallet-http-bridge = wallet.cardano-wallet-http-bridge;
 
 in {
-  inherit cardanoWalletJormungandr cardanoWalletHttpBridge;
+  inherit cardano-wallet-jormungandr cardano-wallet-http-bridge;
 }
