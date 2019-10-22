0cf9bacf6c552a1d8af0daedefcf9351614e21e9
Samuel Leathers <samuel.leathers@iohk.io>
2019-10-22 04:29:11 -0400

    rust: bump stable

--- overlays/rust/mozilla.nix
+++ overlays/rust/mozilla.nix
@@ -3,8 +3,8 @@ let
   rustOverlay = fetchFromGitHub {
     owner = "mozilla";
     repo = "nixpkgs-mozilla";
-    rev = "b52a8b7de89b1fac49302cbaffd4caed4551515f";
-    sha256 = "1np4fmcrg6kwlmairyacvhprqixrk7x9h89k813safnlgbgqwrqb";
+    rev = "d46240e8755d91bc36c0c38621af72bf5c489e13";
+    sha256 = "0icws1cbdscic8s8lx292chvh3fkkbjp571j89lmmha7vl2n71jg";
   };
 in
   import (rustOverlay + "/rust-overlay.nix")
