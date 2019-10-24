214cc7666ced81504ac8b2688aad95f5589405c2
Samuel Leathers <samuel.leathers@iohk.io>
2019-10-24 22:59:29 +0200

    jormungandr: 0.7.0.dev -> 0.7.0-rc1
    (cherry picked from commit b1c35ab8863a081bbdc21919dda1afa868adc76e)

--- overlays/rust/jormungandr.nix
+++ overlays/rust/jormungandr.nix
@@ -15,9 +15,9 @@
 ,  ... }:
 let
   release = {
-    version = "0.7.0-alpha.dev.1";
-    sha256 = "0r3icx42glrpa68sjxz4gr0z5660gh4n79lncy720s04cmgjcjci";
-    cargoSha256 = "0f9b2lr2xxlcn9j33b5ahzbndz6sjm8ybhqm472bv5hzisqm4lg4";
+    version = "0.7.0-rc1";
+    sha256 = "02ihnq7b32rwx7ychrj76rin1z3s9np5yjylppxm0qp5sjkik9ff";
+    cargoSha256 = "1pp829azj6aw68ba637rm852sj61nxznxfzrlqs9ds6adk9h7abs";
   };
 
   master = {
