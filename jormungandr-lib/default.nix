9222070914a0c0e6163fbf45204fab02e06ed8f6
Michael Fellinger <michael.fellinger@iohk.io>
2019-12-02 17:41:45 +0100

    set charset

--- jormungandr-lib/default.nix
+++ jormungandr-lib/default.nix
@@ -117,6 +117,7 @@ let
     <html>
       <head>
         <title>Jörmungandr Status</title>
+        <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
         <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
