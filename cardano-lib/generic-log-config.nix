b1423273cc26f9547908bc21f5df614dfa45d0fe
Samuel Leathers <samuel.leathers@iohk.io>
2019-12-06 12:36:16 -0500

    hasPrometheus: support IP/port combination

--- cardano-lib/generic-log-config.nix
+++ cardano-lib/generic-log-config.nix
@@ -36,7 +36,7 @@
 
   # if wanted, the EKG interface is listening on this port:
   hasEKG = 12788;
-  hasPrometheus = 12798;
+  hasPrometheus = [ "localhost" 12798 ];
 
   # here we set up outputs of logging in 'katip':
   setupScribes = [ {
