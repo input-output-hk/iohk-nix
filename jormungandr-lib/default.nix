4c4604bb2cda68d82451d0f384c88a142b85efd2
Michael Fellinger <michael.fellinger@iohk.io>
2020-01-15 15:41:26 +0100

    jormungandr 0.8.6 -> qa

--- jormungandr-lib/default.nix
+++ jormungandr-lib/default.nix
@@ -344,7 +344,7 @@ let
     };
 
     qa = {
-      packages = packages.v0_8_5;
+      packages = packages.v0_8_6;
       genesisHash = "b5ada4577e10513240a5457490bd855c350b378fcd762d4354b443ba49b42dda";
       genesisFile = ./genesis-qa.yaml;
       registryUrl = "https://explorer.qa.jormungandr-testnet.iohkdev.io/stakepool-registry/registry.zip";
