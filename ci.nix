6981dbb2fd79480e0ffa053bc5d64052de3a38ba
Michael Fellinger <michael.fellinger@iohk.io>
2020-01-22 14:17:14 +0100

    add ci.nix

---
+++ ci.nix
@@ -0,0 +1,13 @@
+let
+  sources = import ./nix/sources.nix;
+  nixpkgs = import sources.nixpkgs { };
+  iohk-nix = import ./. { };
+  inherit (nixpkgs.lib) flatten mapAttrs;
+
+in {
+  jormungandr = nixpkgs.recurseIntoAttrs (mapAttrs (name: env:
+    nixpkgs.recurseIntoAttrs {
+      inherit (env.packages) jcli jcli-debug jormungandr jormungandr-debug;
+    }) iohk-nix.jormungandrLib.environments);
+  niv = iohk-nix.niv;
+}
