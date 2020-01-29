ea7561bfb12657abd1a3363673f33f5414173d1b
Samuel Leathers <samuel.leathers@iohk.io>
2020-01-28 23:53:54 -0500

    move cabal-project-regenerate to bin

--- cabal-project-regenerate/default.nix
+++ cabal-project-regenerate/default.nix
@@ -2,10 +2,10 @@
 let
   python = python3;
 in runCommand "cabal-project-regenerate" {} ''
-  mkdir -p $out
+  mkdir -p $out/bin
   cp ${./cabal-project-regenerate.py} $out/cabal-project-regenerate.py
-  cat << EOF > $out/cabal-project-regenerate
+  cat << EOF > $out/bin/cabal-project-regenerate
   ${python}/bin/python $out/cabal-project-regenerate.py
   EOF
-  chmod +x $out/cabal-project-regenerate
+  chmod +x $out/bin/cabal-project-regenerate
 ''
