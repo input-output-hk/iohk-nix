5fe6f133eea0414265d32277fcbc8bca9a16a27c
Samuel Leathers <samuel.leathers@iohk.io>
2020-07-21 08:41:56 -0400

    cardanoLib: set correct shelley genesis hashes

--- cardano-lib/shelley_staging-config.nix
+++ cardano-lib/shelley_staging-config.nix
@@ -9,7 +9,7 @@
   ByronGenesisFile = ./shelley_staging-byron-genesis.json;
   ByronGenesisHash = "82995abf3e0e0f8ab9a6448875536a1cba305f3ddde18cd5ff54c32d7a5978c6";
   ShelleyGenesisFile = ./shelley_staging-shelley-genesis.json;
-  ShelleyGenesisHash = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+  ShelleyGenesisHash = "0e7f08a8c79674cf91c64f8dc25557b046fc23c487cd6fc8454ce5357a1d805e";
 
 
   ##### Core protocol parameters #####
