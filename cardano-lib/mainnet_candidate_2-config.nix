b77732bb45fcb72293e5354488f7121a198650cc
John Lotoski <john.lotoski@iohk.io>
2020-07-22 10:59:17 -0400

    Adds mainnet_candidate_2 env

---
+++ cardano-lib/mainnet_candidate_2-config.nix
@@ -0,0 +1,43 @@
+##########################################################
+###############      Mainnet Candidate     ###############
+############### Cardano Node Configuration ###############
+##########################################################
+
+{
+  ##### Locations #####
+
+  ByronGenesisFile = ./mainnet_candidate_2-byron-genesis.json;
+  ByronGenesisHash = "7e5a9acd813b0bbc059ab50a33fc7441efb1fc887bf8d84ee87d84c58e3d4636";
+  ShelleyGenesisFile = ./mainnet_candidate_2-shelley-genesis.json;
+  ShelleyGenesisHash = "d1738667a9f7ca6ceb28889e4a713bf146b98123dc06937438c3bb7c64a391e7";
+
+
+  ##### Core protocol parameters #####
+
+  # This is the instance of the Ouroboros family that we are running.
+  # The node also supports various test and mock instances.
+  # "RealPBFT" is the real (ie not mock) (permissive) OBFT protocol, which
+  # is what we use on mainnet in Byron era.
+  Protocol = "Cardano";
+
+  PBftSignatureThreshold = 0.9;
+
+  # The mainnet candidate utilizes same addresses as mainnet
+  RequiresNetworkMagic = "RequiresNoMagic";
+
+  MaxKnownMajorProtocolVersion = 2;
+
+
+  ##### Update system parameters #####
+
+  # This protocol version number gets used by by block producing nodes as part
+  # part of the system for agreeing on and synchronising protocol updates.
+  LastKnownBlockVersion-Major = 0;
+  LastKnownBlockVersion-Minor = 0;
+  LastKnownBlockVersion-Alt = 0;
+
+  # In the Byron era some software versions are also published on the chain.
+  # We do this only for Byron compatibility now.
+  ApplicationName = "cardano-sl";
+  ApplicationVersion = 0;
+}
