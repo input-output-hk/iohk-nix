51a019fc25e4b645b498f3d53af07549d8fae5f4
John Lotoski <john.lotoski@iohk.io>
2020-07-23 09:36:48 -0400

    Updates mainnet-shelley-genesis.json
    * Per PR https://github.com/input-output-hk/cardano-node/pull/1521

--- cardano-lib/mainnet-config.nix
+++ cardano-lib/mainnet-config.nix
@@ -9,7 +9,7 @@
   ByronGenesisFile = ./mainnet-byron-genesis.json;
   ByronGenesisHash = "5f20df933584822601f9e3f8c024eb5eb252fe8cefb24d1317dc3d432e940ebb";
   ShelleyGenesisFile = ./mainnet-shelley-genesis.json;
-  ShelleyGenesisHash = "3cc503155a18726c054fd847e0534f6cceb6e60778fbe30b1445a8006d76a4fb";
+  ShelleyGenesisHash = "1a3be38bcbb7911969283716ad7aa550250226b76a61fc51cc9a9a35d9276d81";
 
 
   ##### Core protocol parameters #####
