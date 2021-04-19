f83e8c4bb2630310b7b67603b90bab746fcb15da
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2021-04-19 16:20:20 +0200

    Add cardano-lib overlay to flake

--- flake.nix
+++ flake.nix
@@ -6,6 +6,9 @@
     overlays = {
       crypto = import ./overlays/crypto;
       haskell-nix-extra = import ./overlays/haskell-nix-extra;
+      cardano-lib = (final: prev: {
+        cardanoLib = final.callPackage ./cardano-lib {};
+      });
     };
 
   };
