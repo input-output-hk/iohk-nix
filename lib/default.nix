079fedec65333c4abce73d51310e5fa37b27af65
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2021-05-04 11:08:20 +0200

    evalService: more versatile customConfig support.

--- lib/default.nix
+++ lib/default.nix
@@ -14,17 +14,28 @@ lib: with lib; rec {
     in if m == null then [] else [ (nameValuePair (head m) p.${n}) ]
   ) (attrNames p));
 
-  evalService = { pkgs, serviceName, modules, customConfig }:
-    let customConfig' = if (isAttrs customConfig) && !(customConfig ? service) then {
-      services.${serviceName} = customConfig;
-    } else customConfig;
+  evalService = { pkgs, serviceName, modules, customConfigs }:
+    let mkConfig = c: if (isAttrs c)
+    then (
+      if c ? nixosModules.${serviceName}
+      then c.nixosModules.${serviceName}
+      else if c ? nixosModule
+      then mkConfig c.nixosModule
+      else if c ? services.${serviceName} then {
+        services.${serviceName} = c.services.${serviceName};
+      } else {options, ...}: {
+        services.${serviceName} =
+          let validOptions = attrNames options.services.${serviceName};
+          in filterAttrs (n: _: elem n validOptions) c;
+      })
+    else c;
+
     in (lib.modules.evalModules {
       prefix = [];
       modules = modules ++ [
         { services.${serviceName}.enable = true; }
-        customConfig'
         systemdCompatModule
-      ];
+      ] ++ (map mkConfig customConfigs);
       args = { inherit pkgs; };
       check = false;
     }).config.services.${serviceName};
