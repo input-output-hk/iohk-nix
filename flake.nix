3d984c904d6d7f3f2f06c9c1ae8b1180c1a68d5d
Moritz Angermann <moritz.angermann@gmail.com>
2022-09-21 13:48:20 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -21,6 +21,7 @@
         oldR = (final: prev: {
           R_4_1_3 = final.R.overrideDerivation (old: rec {
             version = "4.1.3";
+            patches = []; # upstream patches will most likely break this build, as they are specific to a different version.
             src = final.fetchurl {
               url = "https://cran.r-project.org/src/base/R-${final.lib.versions.major version}/${old.pname}-${version}.tar.gz";
               sha256 = "sha256-Ff9bMzxhCUBgsqUunB2OxVzELdAp45yiKr2qkJUm/tY="; };
