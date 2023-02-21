8067a7f8b0689ae8a02e43203dc79e021ce144b9
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2023-02-21 13:40:31 +0100

    Use generic python3 instead of python37 (removed from nixpkgs-unstable)

--- ci/sha256map-regenerate/default.nix
+++ ci/sha256map-regenerate/default.nix
@@ -1,6 +1,6 @@
 { stdenv
 , lib
-, python37
+, python3
 , git
 , nix-prefetch-git
 }:
@@ -14,7 +14,7 @@ stdenv.mkDerivation {
     bin="$out/bin/$name"
     mkdir -p $out/bin
     sed \
-      -e '1c #!${python37}/bin/python' \
+      -e '1c #!${python3}/bin/python' \
       -e '2i import os; os.environ["PATH"] += os.pathsep + os.pathsep.join([${lib.concatMapStringsSep ", " (p: "\"${p}/bin\"") [ git nix-prefetch-git ]}])' \
       < "$src" \
       > "$bin"
