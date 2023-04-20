945a6bee2e50b4a8a2d105b1c7e0ba0e436e0f78
Moritz Angermann <moritz.angermann@gmail.com>
2023-04-20 08:38:47 +0800

    Add haskell-nix pkg-config mapping for custom libraries.

--- overlays/crypto/default.nix
+++ overlays/crypto/default.nix
@@ -2,3 +2,13 @@ final: prev: {
   libsodium-vrf = final.callPackage ./libsodium.nix {};
   blst = final.callPackage ./blst.nix {};
 }
+# Make these libraries also available to haskell-nix's pkg-config
+# map when solving for dependencies with cabal.
+// final.lib.optionalAttrs (prev ? haskell-nix) {
+  haskell-nix = prev.haskell-nix // {
+    extraPkgconfigMappings = super.haskell-nix.extraPkgconfigMappings // {
+      "libblst" = [ "blst" ];
+      "libsodium-vrf" = [ "libsodium-vrf" ];
+    };
+  };
+}
