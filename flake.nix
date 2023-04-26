cb3111ad8501e129c5f0b6f31dd51754d3bba564
Moritz Angermann <moritz.angermann@gmail.com>
2023-04-26 10:58:46 +0000

    _very ugly hack_ around debian version numbers.

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
       mkDebianPkg = prefix: drv: let
         control = pkgs.writeText "control" ''
         Package: ${drv.pname}
-        Version: ${drv.version}
+        Version: ${if drv.version == "unstable-2022-02-06" then "2022-02-06-unstable" else drv.version}
         Architecture: amd64
         Maintainer: IOG <engineering@iog.io>
         Description: ${drv.meta.description}
