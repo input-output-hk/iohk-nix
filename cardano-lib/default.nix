49f80e6cb415811053b63baeedca8e1a561b9c3c
Samuel Leathers <samuel.leathers@iohk.io>
2023-05-04 11:01:03 -0400

    cleanup legacy environments
    * fixes node config experimental parameter names
    * stops referencing mainnet conway genesis from all envs

--- cardano-lib/default.nix
+++ cardano-lib/default.nix
@@ -60,56 +60,6 @@ let
       explorerConfig = mkExplorerConfig "mainnet" nodeConfig;
       usePeersFromLedgerAfterSlot = 29691317;
     };
-    staging = rec {
-      useByronWallet = true;
-      relaysNew = "relays.staging.cardano.org";
-      explorerUrl = "https://explorer.staging.cardano.org";
-      smashUrl = "https://smash.staging.cardano.org";
-      metadataUrl = "https://metadata.cardano-testnet.iohkdev.io";
-      edgeNodes = [
-        "3.125.10.61"
-        "52.192.59.170"
-        "18.136.145.112"
-      ];
-      edgePort = 3001;
-      confKey = "mainnet_dryrun_full";
-      private = false;
-      networkConfig = import ./staging-config.nix;
-      nodeConfig = networkConfig // defaultLogConfig;
-      consensusProtocol = networkConfig.Protocol;
-      submitApiConfig = {
-        GenesisHash = nodeConfig.ByronGenesisHash;
-        inherit (networkConfig) RequiresNetworkMagic;
-      } // defaultExplorerLogConfig;
-      explorerConfig = mkExplorerConfig "staging" nodeConfig;
-      usePeersFromLedgerAfterSlot = 29444240;
-    };
-    testnet = rec {
-      useByronWallet = true;
-      relays = "relays.cardano-testnet.iohkdev.io";
-      relaysNew = "relays-new.cardano-testnet.iohkdev.io";
-      explorerUrl = "https://explorer.cardano-testnet.iohkdev.io";
-      smashUrl = "https://smash.cardano-testnet.iohkdev.io";
-      metadataUrl = "https://metadata.cardano-testnet.iohkdev.io";
-      edgeNodes = [
-        "3.125.94.58"
-        "18.176.19.63"
-        "13.251.186.36"
-        "3.135.95.164"
-      ];
-      edgePort = 3001;
-      confKey = "testnet_full";
-      private = false;
-      networkConfig = import ./testnet-config.nix;
-      nodeConfig = networkConfig // defaultLogConfig;
-      consensusProtocol = networkConfig.Protocol;
-      submitApiConfig = {
-        GenesisHash = nodeConfig.ByronGenesisHash;
-        inherit (networkConfig) RequiresNetworkMagic;
-      } // defaultExplorerLogConfig;
-      explorerConfig = mkExplorerConfig "testnet" nodeConfig;
-      usePeersFromLedgerAfterSlot = 26888469;
-    };
     p2p = rec {
       useByronWallet = false;
       private = false;
@@ -124,34 +74,6 @@ let
       explorerConfig = mkExplorerConfig "p2p" nodeConfig;
       usePeersFromLedgerAfterSlot = 14680;
     };
-    alonzo-purple = rec {
-      useByronWallet = false;
-      private = false;
-      relaysNew = "relays.alonzo-purple.dev.cardano.org";
-      explorerUrl = "https://explorer.alonzo-purple.dev.cardano.org";
-      smashUrl = "https://smash.alonzo-purple.dev.cardano.org";
-      metadataUrl = "https://metadata.cardano-testnet.iohkdev.io";
-      networkConfig = import ./alonzo-purple-config.nix;
-      consensusProtocol = networkConfig.Protocol;
-      nodeConfig = defaultLogConfig // networkConfig;
-      edgePort = 3001;
-      explorerConfig = mkExplorerConfig "alonzo-purple" nodeConfig;
-    };
-    marlowe-pioneers = rec {
-      useByronWallet = false;
-      private = false;
-      relaysNew = "relays.marlowe-pioneers.dev.cardano.org";
-      explorerUrl = "https://explorer.marlowe-pioneers.dev.cardano.org";
-      smashUrl = "https://smash.marlowe-pioneers.dev.cardano.org";
-      metadataUrl = "https://metadata.cardano-testnet.iohkdev.io";
-      networkConfig = import ./marlowe-pioneers-config.nix;
-      consensusProtocol = networkConfig.Protocol;
-      nodeConfig = defaultLogConfig // networkConfig;
-      edgePort = 3001;
-      explorerConfig = mkExplorerConfig "marlowe-pioneers" nodeConfig;
-      usePeersFromLedgerAfterSlot = 40000;
-    };
-    # used for daedalus/cardano-wallet for local development
     shelley_qa = rec {
       useByronWallet = false;
       private = false;
@@ -165,35 +87,6 @@ let
       edgePort = 3001;
       explorerConfig = mkExplorerConfig "shelley_qa" nodeConfig;
       usePeersFromLedgerAfterSlot = 23574838;
-    };
-    # used for SRE development
-    sre = rec {
-      useByronWallet = false;
-      private = false;
-      relaysNew = "relays.sre.dev.cardano.org";
-      explorerUrl = "https://explorer.sre.dev.cardano.org";
-      smashUrl = "https://smash.sre.dev.cardano.org";
-      metadataUrl = "https://metadata.cardano-testnet.iohkdev.io";
-      networkConfig = import ./shelley_qa-config.nix;
-      consensusProtocol = networkConfig.Protocol;
-      nodeConfig = defaultLogConfig // networkConfig;
-      edgePort = 3001;
-      explorerConfig = mkExplorerConfig "sre" nodeConfig;
-      usePeersFromLedgerAfterSlot = 122760;
-    };
-    vasil-dev = rec {
-      useByronWallet = false;
-      private = false;
-      relaysNew = "vasil-dev-node.world.dev.cardano.org";
-      explorerUrl = "https://explorer.vasil-dev.world.dev.cardano.org";
-      smashUrl = "https://smash.vasil-dev.world.dev.cardano.org";
-      metadataUrl = "https://metadata.cardano-testnet.iohkdev.io";
-      networkConfig = import ./vasil-dev-config.nix;
-      consensusProtocol = networkConfig.Protocol;
-      nodeConfig = defaultLogConfig // networkConfig;
-      edgePort = 30001;
-      explorerConfig = mkExplorerConfig "vasil-dev" nodeConfig;
-      usePeersFromLedgerAfterSlot = 60000;
     };
     preprod = rec {
       useByronWallet = false;
--- cardano-lib/p2p-config.nix
+++ cardano-lib/p2p-config.nix
@@ -12,7 +12,7 @@
   ShelleyGenesisHash = "a0e8e5520ab7c452c4e36020ded12c791a10abd7ca25c083af6149fe269ddb67";
   AlonzoGenesisFile = ./p2p + "/alonzo-genesis.json";
   AlonzoGenesisHash = "7e94a15f55d1e82d10f09203fa1d40f8eede58fd8066542cf6566008068ed874";
-  ConwayGenesisFile = ./mainnet + "/conway-genesis.json";
+  ConwayGenesisFile = ./p2p + "/conway-genesis.json";
   ConwayGenesisHash = "f28f1c1280ea0d32f8cd3143e268650d6c1a8e221522ce4a7d20d62fc09783e1";
 
   ##### Core protocol parameters #####
@@ -35,7 +35,6 @@
   ### P2P
 
   EnableP2P = true;
-  TestEnableDevelopmentNetworkProtocols = true;
   TraceInboundGovernorCounters = true;
 
   MaxKnownMajorProtocolVersion = 2;
