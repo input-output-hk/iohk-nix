56cd08f5b7795f3c663f10d3606cd5af556f1b72
John Lotoski <john.lotoski@iohk.io>
2023-10-28 03:13:48 -0500

    imp: update private chain

--- cardano-lib/private-config.nix
+++ cardano-lib/private-config.nix
@@ -7,11 +7,11 @@
   ##### Locations #####
 
   ByronGenesisFile = ./private + "/byron-genesis.json";
-  ByronGenesisHash = "9a2d021950a717b48ee26079178ea0740e2b67ee9bcfcf4623f81f7c66d7c583";
+  ByronGenesisHash = "1c49093e43757a7f5d55791ee8682d2681e7a291b10dadc663ac1889b3f16984";
   ShelleyGenesisFile = ./private + "/shelley-genesis.json";
-  ShelleyGenesisHash = "d9d01796f5cfab8da3833dd98d15b3967fecb52ab75df9f0267be7888d535ead";
+  ShelleyGenesisHash = "136177de30fc5d431cc80f5789089486c13629e589dd705aa4254f765a84582b";
   AlonzoGenesisFile = ./private + "/alonzo-genesis.json";
-  AlonzoGenesisHash = "7e94a15f55d1e82d10f09203fa1d40f8eede58fd8066542cf6566008068ed874";
+  AlonzoGenesisHash = "8bedcaea62107d8a79ed5293b0027b3f8706a4bc2422f33380cb1fd01c6fa6ec";
   ConwayGenesisFile = ./private + "/conway-genesis.json";
   ConwayGenesisHash = "f28f1c1280ea0d32f8cd3143e268650d6c1a8e221522ce4a7d20d62fc09783e1";
 
@@ -24,15 +24,14 @@
   TargetNumberOfKnownPeers = 100;
   TargetNumberOfRootPeers = 100;
   ExperimentalHardForksEnabled = true;
-  ExperimentalProtocolsEnabled = false;
+  ExperimentalProtocolsEnabled = true;
   TestShelleyHardForkAtEpoch = 0;
   TestAllegraHardForkAtEpoch = 0;
-  TestMaryHardForkAtEpoch = 0;
   TestAlonzoHardForkAtEpoch = 0;
-
+  TestMaryHardForkAtEpoch = 0;
 
   ##### Update system Parameters #####
-  LastKnownBlockVersion-Major = 6;
-  LastKnownBlockVersion-Minor = 0;
+  LastKnownBlockVersion-Major = 3;
+  LastKnownBlockVersion-Minor = 1;
   LastKnownBlockVersion-Alt = 0;
 }
