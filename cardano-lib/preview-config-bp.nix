59e16a0c8fa98185cf2533b4aa65886ce9b0a595
Samuel Leathers <samuel.leathers@iohk.io>
2024-06-18 06:31:16 -0400

    node-8.12: remove conway genesis for preview/preprod/mainnet

--- cardano-lib/preview-config-bp.nix
+++ cardano-lib/preview-config-bp.nix
@@ -12,8 +12,6 @@
   ShelleyGenesisHash = "363498d1024f84bb39d3fa9593ce391483cb40d479b87233f868d6e57c3a400d";
   AlonzoGenesisFile = ./preview + "/alonzo-genesis.json";
   AlonzoGenesisHash = "7e94a15f55d1e82d10f09203fa1d40f8eede58fd8066542cf6566008068ed874";
-  ConwayGenesisFile = ./preview + "/conway-genesis.json";
-  ConwayGenesisHash = "91bedad42212c07f6abdafedb7e7c8577fbd07152c695ffae1ab2a528741c6e4";
 
   ### Core protocol parameters #####
   Protocol = "Cardano";
