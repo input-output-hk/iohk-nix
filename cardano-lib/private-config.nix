6aeccc5fb8e55e146878cc9f08e83308f2a23da4
Samuel Leathers <samuel.leathers@iohk.io>
2024-07-15 20:05:57 -0400

    fix genesis files for private demo

--- cardano-lib/private-config.nix
+++ cardano-lib/private-config.nix
@@ -9,11 +9,11 @@
   ByronGenesisFile = ./private + "/byron-genesis.json";
   ByronGenesisHash = "200222f56d3582c58af09ba58d53e294fcca5de2fc6a27913803e427cdf147f3";
   ShelleyGenesisFile = ./private + "/shelley-genesis.json";
-  ShelleyGenesisHash = "ae9da178ddecfbde046d2c59d4afd087bafd6e2ff04532e6077eab7f8098fa03";
+  ShelleyGenesisHash = "34e2d83e3dc2fb33ca2b41bd4314dc8e264d877034e4331a23cd7e58e530561e";
   AlonzoGenesisFile = ./private + "/alonzo-genesis.json";
-  AlonzoGenesisHash = "8bedcaea62107d8a79ed5293b0027b3f8706a4bc2422f33380cb1fd01c6fa6ec";
+  AlonzoGenesisHash = "0cd6973c5c4a2a8754f82b4807f3f9d6c837ef169f42da9f4ac0917d2275579a";
   ConwayGenesisFile = ./private + "/conway-genesis.json";
-  ConwayGenesisHash = "75ff70079cca9f3fd2109d89d5770e96f6f79827dba7b410a69ee90066b78c5f";
+  ConwayGenesisHash = "5c9f9defd945ea6db33941cfef846b1451286dc054de2104f6163803ca74a3e6";
 
   ### Core protocol parameters #####
   Protocol = "Cardano";
