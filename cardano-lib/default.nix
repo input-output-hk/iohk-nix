72c5068f50ac46ec7baa27706ae6f34117afcd48
Samuel Leathers <samuel.leathers@iohk.io>
2024-07-16 00:05:12 -0400

    -1 ledger after peers

--- cardano-lib/default.nix
+++ cardano-lib/default.nix
@@ -277,7 +277,7 @@ let
       edgePort = 3001;
       networkConfig = import ./private-config.nix // minNodeVersion;
       networkConfigBp = import ./private-config-bp.nix // minNodeVersion;
-      usePeersFromLedgerAfterSlot = 10000000000;
+      usePeersFromLedgerAfterSlot = -1;
     };
   };
 
