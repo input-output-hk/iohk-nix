62e3bbc30151587d71d67e30af40c35bd56ee80a
John Lotoski <john.lotoski@iohk.io>
2025-03-31 20:53:36 -0500

    imp: add support for GenesisMode config

--- cardano-lib/preview-config.nix
+++ cardano-lib/preview-config.nix
@@ -14,9 +14,12 @@
   ShelleyGenesisHash = "363498d1024f84bb39d3fa9593ce391483cb40d479b87233f868d6e57c3a400d";
   AlonzoGenesisFile = ./preview + "/alonzo-genesis.json";
   AlonzoGenesisHash = "7e94a15f55d1e82d10f09203fa1d40f8eede58fd8066542cf6566008068ed874";
+  CheckPointsFile = "placeholder str: ./preview + ...";
+  CheckPointsHash = "...";
 
   ### Core protocol parameters #####
   Protocol = "Cardano";
+
   RequiresNetworkMagic = "RequiresMagic";
   EnableP2P = true;
   PeerSharing = true;
@@ -31,7 +34,20 @@
   TestAlonzoHardForkAtEpoch = 0;
   TestMaryHardForkAtEpoch = 0;
 
-  ##### Update system Parameters #####
+  # The consensus mode.  If set to "GenesisMode", the CheckPointsFile and
+  # CheckpointsHash value above will be used and an absolute path to a peer
+  # snapshot file will need to be declared in the p2p topology file under key
+  # `peerSnapshotFile`.
+  ConsensusMode = "PraosMode";
+
+  # Default parameter values for "GenesisMode"
+  SyncTargetNumberOfActivePeers = 0;
+  SyncTargetNumberOfActiveBigLedgerPeers = 30;
+  SyncTargetNumberOfEstablishedBigLedgerPeers = 50;
+  SyncTargetNumberOfKnownBigLedgerPeers = 100;
+  MinBigLedgerPeersForTrustedState = 5;
+
+  ##### Update system parameters #####
 
   LastKnownBlockVersion-Major = 3;
   LastKnownBlockVersion-Minor = 1;
