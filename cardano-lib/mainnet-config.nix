e46b31587a981b23a7268c9526219e16bfe2b332
John Lotoski <john.lotoski@iohk.io>
2026-05-11 18:37:46 -0500

    cardanoLib: update the LedgerDB config key per env

--- cardano-lib/mainnet-config.nix
+++ cardano-lib/mainnet-config.nix
@@ -46,7 +46,6 @@ with builtins; {
   # Peer Sharing Mode, Network Deadline and Sync Target Configuration
   # To avoid config drift these will use ouroboros-network defaults which may change over time.
   # The values shown below are examples.
-  # See: https://developers.cardano.org/docs/get-started/cardano-node/p2p
   #
   # MinBigLedgerPeersForTrustedState = 5;
   # SyncTargetNumberOfActiveBigLedgerPeers = 30;
@@ -75,19 +74,36 @@ with builtins; {
   # Additional configuration options can be found at:
   # https://ouroboros-consensus.cardano.intersectmbo.org/docs/for-developers/utxo-hd/migrating
   LedgerDB = {
-    # The time interval between snapshots, in seconds.
-    SnapshotInterval = (fromJSON (readFile ./mainnet/shelley-genesis.json)).securityParam * 2;
-
-    # The number of disk snapshots to keep.
-    NumOfDiskSnapshots = 2;
-
     # When querying the store for a big range of UTxOs (such as with
     # QueryUTxOByAddress), the store will be read in batches of this size.
     QueryBatchSize = 100000;
 
     # The backend can either be in memory with `V2InMemory` or on disk with
-    # `V1LMDB`.
+    # `V2LSM`.
     Backend = "V2InMemory";
+
+    # Instead of an object (attribute set) with individual options, a
+    # predefined snapshot policy can be selected by name, e.g.
+    # `Snapshots = "Mithril";`.
+    Snapshots = {
+      # The snapshot interval in slots.
+      SnapshotInterval = (fromJSON (readFile ./mainnet/shelley-genesis.json)).securityParam * 2;
+
+      # Start taking the snaphots at a slot offset.
+      # SlotOffset = 172800;
+
+      # A minimum duration between snapshots, in seconds (used to avoid excessive snapshots while syncing).
+      # Default is 10 minutes.
+      # RateLimit = 600;
+
+      # Randomised snapshot delay range, in seconds.
+      # Both Min and Max need to be specified, otherwise the default delay of (5min, 10min) will be used.
+      # MinDelay = 300;
+      # MaxDelay = 600;
+
+      # The number of disk snapshots to keep.
+      NumOfDiskSnapshots = 2;
+    };
   };
 
   # Environment customized tracing options
